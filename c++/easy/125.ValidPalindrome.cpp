394b5fbe5b6e5f4656a2347ce8cc812805cbea3e
admin <nnamdichibuzor777@gmail.com>
2025-06-06 15:16:16 -0700

    update-125-valid-palindrome

--- c++/easy/125.ValidPalindrome.cpp
+++ c++/easy/125.ValidPalindrome.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <cctype>
 using std::string, std::cout, std::endl;
+
 
 class Solution {
 public:
+    std::string formatString(const std::string& input) {
+        std::string result;
+        for (char ch : input) {
+            if (std::isalnum(ch)) {
+                result.push_back(std::tolower(ch));
+            }
+        }
+        return result;
+    }
+
     bool isPalindrome(string s) {
+
+        s = formatString(s);
+        cout << s << endl;
+
         int left = 0;
         int right = s.length()-1;
         char leftChar;
