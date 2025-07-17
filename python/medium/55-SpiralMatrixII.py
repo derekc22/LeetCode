3016c6504fba536d8047522314de9e44c563683f
admin <nnamdichibuzor777@gmail.com>
2025-07-16 19:38:11 -0700

    lol

--- python/medium/55-SpiralMatrixII.py
+++ python/medium/55-SpiralMatrixII.py
@@ -1,18 +1,15 @@
 
 class Solution:
-    # def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-    def spiralOrder(self, matrix):
+    # def generateMatrix(self, n: int) -> List[List[int]]:
+    def generateMatrix(self, n):
+
         """
-            This solution references this implementation:
+            This solution is a modification of '54-SpiralMatrix.py', which is derived from this reference:
             https://www.youtube.com/watch?v=1ZGJzvkcLsA
-        
-        """
-        
-        m = len(matrix)
-        n = len(matrix[0])
-        
+        """        
+
         top_ptr = 0
-        bottom_ptr = m-1
+        bottom_ptr = n-1
         left_ptr = 0
         right_ptr = n-1
         
@@ -22,33 +19,40 @@ class Solution:
         # 2: right->left
         # 3: bottom->top
         
-        spiral = []
+        ni = 1
+        
+        matrix = [[0]*n for _ in range(n)]
+        # You CAN NOT use [[0]*n]*n here as the outer '*' operator will create n shallow copies of each [0]*n row
+        # Thus, each row will point to the same address in memory...
+        # Meaning that all rows in the matrix will be altered when just one is changed
         while left_ptr <= right_ptr and top_ptr <= bottom_ptr:
             if direction == 0:
                 for i in range(left_ptr, right_ptr+1):
-                    spiral.append(matrix[top_ptr][i])
+                    matrix[top_ptr][i] = ni
+                    ni+=1
                 top_ptr += 1
             if direction == 1:
                 for i in range(top_ptr, bottom_ptr+1):
-                    spiral.append(matrix[i][right_ptr])
+                    matrix[i][right_ptr] = ni
+                    ni+=1
                 right_ptr -= 1
             if direction == 2:
                 for i in range(right_ptr, left_ptr-1, -1):
-                    spiral.append(matrix[bottom_ptr][i])
+                    matrix[bottom_ptr][i] = ni
+                    ni+=1
                 bottom_ptr -= 1
             if direction == 3:
                 for i in range(bottom_ptr, top_ptr-1, -1):
-                    spiral.append(matrix[i][left_ptr])
+                    matrix[i][left_ptr] = ni
+                    ni+=1
                 left_ptr += 1
                 
             direction = (direction + 1) % 4
             
-        return spiral
+        return matrix
             
 
 
 
 sol = Solution()
-# matrix = [[1,2,3],[4,5,6],[7,8,9]]
-matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
-print(sol.spiralOrder(matrix))
+print(sol.generateMatrix(3))
